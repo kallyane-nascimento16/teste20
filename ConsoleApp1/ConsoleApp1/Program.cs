f3a45b18810a8d0da86ab90592ba2dbe9144dee2
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:44:50 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.AccessControl;
+
+public class condicaoSE
+{
+
+
+
+
+
+    public static void Main(string[] args)
+    {
+        //quero programa para validar a idade de uma pessoa, e saber se ela é maior de idade ou nao
+        int idade;
+
+        Console.WriteLine("qual sua idade!! ");
+        idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("_________________________");
+            Console.WriteLine("Opa.. voce e maior de idade!");
+            Console.WriteLine("____________________________");
+
+        }
+        else
+        {
+            Console.WriteLine("Opa.. nao tem altorizacao!!");
+            Console.WriteLine("__________________________");
+
+
+        }
+
+
+
+
+        }
+
+
+    }
+
